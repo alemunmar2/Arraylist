9defb1d234d17f12f0ce93441f2b31396650f900
alemunmar2 <alemunmar2@alu.edu.gva.es>
2024-12-03 20:23:46 +0100

    Ejercicio1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+
+   ArrayList<String> marcas = new ArrayList<String>();
+   boolean nada = false;
+   Scanner sc = new Scanner(System.in);
+
+    System.out.println("Dime marcas de coche");
+    String porsi;
+
+    do {
+        porsi = sc.nextLine();
+        if (!porsi.isEmpty())
+        marcas.add(porsi);
+    }while (!porsi.isEmpty());
+
+        Collections.sort(marcas);
+        System.out.println(marcas);
+
+
+
+    }
+}
